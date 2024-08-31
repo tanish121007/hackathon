cf8a40f79184cf6961927f134e1bd74a71ea7278
tanish121007 <tdshah121007@gmail.com>
2024-08-31 23:23:00 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer>
+    <p>&copy; 2024 Explore Indian Culture and Heritage. All rights reserved.</p>
+</footer>
+<!-- Bootstrap JS and dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script>
+    function myFunction() {
+        var x = document.getElementById("navbarNav");
+        if (x.className === "navbar-nav") {
+            x.className += " responsive";
+        } else {
+            x.className = "navbar-nav";
+        }
+    }
+</script>
+</body>
+</html>
